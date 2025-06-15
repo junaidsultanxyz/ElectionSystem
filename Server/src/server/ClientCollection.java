66f1dd55e6b90cfbfe60de249f74b1f054a20c43
junaidxyz <jsultanwork@gmail.com>
2025-06-16 00:55:34 +0500

    client handler

---
+++ Server/src/server/ClientCollection.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package server;
+
+import java.util.ArrayList;
+
+public class ClientCollection {
+    public static ArrayList<ClientHandler> clientHandler = new ArrayList<>();
+}
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -1,10 +1,52 @@
 
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.sound.sampled.Port;
+
 
 public class Server {
-
-    public static void main(String[] args) {
-
+    private ServerSocket serverSocket;
+    public static final int port = 8085;
+    
+    public Server(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
+    }
+    
+    public void startServer(){
+        try{
+            while (!serverSocket.isClosed())
+            {
+                Socket socket = serverSocket.accept();
+                System.out.println("New Client Connected");
+                ClientHandler clientHandler = new ClientHandler();
+                
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        }
+        catch(IOException e){
+            System.out.println("[ERROR]: " + e.getMessage());
+        }
+    }
+    
+    public void closeServer(){
+        try {
+            if (serverSocket != null){
+                serverSocket.close();
+            }
+        }
+        catch(IOException e){
+            System.out.println("[ERROR]: " + e.getMessage());
+        }
+    }
+    
+    
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        Server server = new Server(serverSocket);
+        server.startServer();
     }
 }
