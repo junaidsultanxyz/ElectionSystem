8ea01f8432dd10c2b2f8b25c03416519bbabe2bd
junaid <jsultanwork@gmail.com>
2025-06-20 14:36:09 +0500

    schema update

--- Database/data.sql
+++ Database/data.sql
@@ -5,8 +5,7 @@ CREATE TABLE voter (
     cnic varchar(13) PRIMARY KEY,
     name varchar(50),
     age int,
-    gender varchar(10),
-    division varchar(100),
+    division_id INT NOT NULL,
     password varchar(50)
 );
 
@@ -26,23 +25,58 @@ CREATE TABLE division (
     id INT PRIMARY KEY,
     city_id VARCHAR(5),
     FOREIGN KEY (city_id) REFERENCES city(id),
-    province_code VARCHAR(5),
-    FOREIGN KEY (province_code) REFERENCES province(code)
 );
 
 CREATE TABLE party (
-    id INT PRIMARY KEY,
-    name VARCHAR(100)
+    code VARCHAR(5) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL UNIQUE
 );
 
 
 
 CREATE TABLE vote (
     id INT PRIMARY KEY,
-    cnic VARCHAR(13),
+    cnic VARCHAR(13) NOT NULL,
     FOREIGN KEY (cnic) REFERENCES voter(cnic),
-    party_id INT,
-    FOREIGN KEY (party_id) REFERENCES party(id),
-    division_id INT,
-    FOREIGN KEY (division_id) REFERENCES division(id)
+    party_code VARCHAR(5) NOT NULL,
+    FOREIGN KEY (party_code) REFERENCES party(code),
+    vote_type ENUM('NA', 'PA') NOT NULL;    
 );
+
+
+
+-- https://www.figma.com/proto/VQqUgIU9P9UPmPwsbEYTAu/tradeyourself-App-ref?node-id=167-1883&t=Wdd8aKCpUWXYtgG5-0&scaling=contain&content-scaling=fixed&page-id=68%3A2
+
+
+
+
+
+
+
+
+
+
+
+NA (WHOLE COUNTRY)
+1-250
+
+
+PUNJAB (PP) (PROVINCE SPECIFIC)
+1-100
+
+
+SINDH (PS) (PROVINCE SPECIFIC)
+1-80
+
+
+
+
+
+
+
+
+
+
+
+
+
