7c257333c52e24d70a5dde9ba529b47fa271b202
junaid <jsultanwork@gmail.com>
2025-06-20 18:20:54 +0500

    server side set

---
+++ Client/src/main/java/com/junaid/client/Client.java
@@ -0,0 +1,30 @@
+
+
+package com.junaid.client;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
+/**
+ *
+ * @author junaidxyz
+ */
+public class Client {
+
+//    public static void main(String[] args) {
+//        
+////        try {
+////            UIManager.setLookAndFeel(new FlatLightLaf());
+////        }
+////        catch (UnsupportedLookAndFeelException ex) {
+////            System.err.println("Failed to initialize LaF");
+////        }
+////
+////        SwingUtilities.invokeLater(() -> {
+////            new MyFrame().setVisible(true);
+////        });
+//    }
+}
